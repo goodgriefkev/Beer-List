4f4d73cc2837995f316ed858a6da76fb540d841c
Kevin Honnoll <khonnoll@gmail.com>
2019-04-28 17:49:44 -0400

    created working queryURL variable

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 console.log('find some beer');
 
-const baseURL = 'https://sandbox-api.brewerydb.com/v2/'
-const apiKey = '?key=4ece60622e673c96654a9c304ae0b66d'
-console.log(baseURL + 'beers/' + apiKey);
-(()=>{
+const baseURL = 'https://sandbox-api.brewerydb.com/v2/';
+const queryType = 'search?q='
+let query = 'lager';
+const apiKey = '&key=4ece60622e673c96654a9c304ae0b66d';
+
+let queryURL = baseURL + queryType + query + apiKey;
+console.log(queryURL);
+
+// const getBeer = () => {
+//   const beerData = $.ajax({
+//     url: queryURL
+//   }).then((beerData) => {
+//     console.log(beerData);
+//   }, (error) => {
+//     console.log(error);
+//   })
+// };
+
+
+$(()=>{
+
+  // getBeer();
 
 });
