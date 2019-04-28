bfac9643341220abeea8df2dd618c4ce81fb797f
Kevin Honnoll <khonnoll@gmail.com>
2019-04-28 14:05:29 -0400

    connected to brewerydb api

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 console.log('find some beer');
+
+const baseURL = 'https://sandbox-api.brewerydb.com/v2/'
+const apiKey = '?key=4ece60622e673c96654a9c304ae0b66d'
+console.log(baseURL + 'beers/' + apiKey);
+(()=>{
+
+});
